006fc2c8c2b6557c22c8e96f6f511d3d515c1057
hafid hidayatullah <hafid@samudrainovasi.com>
2024-06-05 14:09:21 +0700

    fix critical

--- critical_path.go
+++ critical_path.go
@@ -24,6 +24,7 @@ func calculateCriticalComponent(tasks []Task) []Task {
 	}
 	file, _ := os.Create("./fp.gv")
 	_ = draw.DOT(g, file)
+	longestFinish := float64(0)
 	for i := range tasks {
 		var currentPredID []string
 		_ = graph.BFS(g, tasks[i].UniqueID, func(value string) bool {
@@ -38,13 +39,34 @@ func calculateCriticalComponent(tasks []Task) []Task {
 				tasks[taskIndices[currentPredID[j]]].EarlyFinish = tasks[taskIndices[currentPredID[j]]].Duration
 				if len(tasks[taskIndices[currentPredID[j]]].Predecessors) > 0 {
 					for _, predecessor := range tasks[taskIndices[currentPredID[j]]].Predecessors {
-						maxEF := float64(0)
-						tasks[taskIndices[currentPredID[j]]].EarlyStart = tasks[taskIndices[predecessor.UniqueID]].EarlyFinish
-						if maxEF > tasks[taskIndices[predecessor.UniqueID]].EarlyFinish {
-							tasks[taskIndices[predecessor.UniqueID]].EarlyFinish = maxEF
-						} else {
+						lag := float64(0)
+						if predecessor.Lag != "0" {
+							unit, value := splitLag(predecessor.Lag)
+							lag = float64(value)
+							if unit == "mo" {
+								lag = float64(value) * 30
+							}
+						}
+						switch predecessor.Type {
+						case "FS":
+							maxEF := float64(0)
+							tasks[taskIndices[currentPredID[j]]].EarlyStart = tasks[taskIndices[predecessor.UniqueID]].EarlyFinish + float64(lag)
+							if maxEF > tasks[taskIndices[predecessor.UniqueID]].EarlyFinish {
+								tasks[taskIndices[predecessor.UniqueID]].EarlyFinish = maxEF
+							} else {
+								tasks[taskIndices[currentPredID[j]]].EarlyFinish = tasks[taskIndices[currentPredID[j]]].EarlyStart + tasks[taskIndices[currentPredID[j]]].Duration
+								maxEF = tasks[taskIndices[predecessor.UniqueID]].EarlyFinish
+							}
+						case "FF":
+						case "SF":
+							tasks[taskIndices[currentPredID[j]]].EarlyStart = tasks[taskIndices[predecessor.UniqueID]].EarlyStart + float64(lag) - 1
 							tasks[taskIndices[currentPredID[j]]].EarlyFinish = tasks[taskIndices[currentPredID[j]]].EarlyStart + tasks[taskIndices[currentPredID[j]]].Duration
-							maxEF = tasks[taskIndices[predecessor.UniqueID]].EarlyFinish
+						case "SS":
+							tasks[taskIndices[currentPredID[j]]].EarlyStart = tasks[taskIndices[predecessor.UniqueID]].EarlyStart + float64(lag)
+							tasks[taskIndices[currentPredID[j]]].EarlyFinish = tasks[taskIndices[currentPredID[j]]].EarlyStart + tasks[taskIndices[currentPredID[j]]].Duration
+						}
+						if tasks[taskIndices[currentPredID[j]]].EarlyFinish > longestFinish {
+							longestFinish = tasks[taskIndices[currentPredID[j]]].EarlyFinish
 						}
 					}
 				}
@@ -53,8 +75,6 @@ func calculateCriticalComponent(tasks []Task) []Task {
 				// fmt.Println("[ITERATION]", j, "CurrentTask", tasks[j].UniqueID, "Task: ", tasks[2].UniqueID, "ES: ", tasks[2].EarlyStart, "EF: ", tasks[2].EarlyFinish, "Duration: ", tasks[2].Duration)
 				// fmt.Println("[ITERATION]", j, "CurrentTask", tasks[j].UniqueID, "Task: ", tasks[4].UniqueID, "ES: ", tasks[4].EarlyStart, "EF: ", tasks[4].EarlyFinish, "Duration: ", tasks[4].Duration)
 				// fmt.Println("[ITERATION]", j, "CurrentTask", tasks[j].UniqueID, "Task: ", tasks[5].UniqueID, "ES: ", tasks[5].EarlyStart, "EF: ", tasks[5].EarlyFinish, "Duration: ", tasks[5].Duration)
-				// fmt.Println("[ITERATION]", j, "CurrentTask", tasks[j].UniqueID, "Task: ", tasks[6].UniqueID, "ES: ", tasks[6].EarlyStart, "EF: ", tasks[6].EarlyFinish, "Duration: ", tasks[6].Duration)
-				// fmt.Println("[ITERATION]", j, "CurrentTask", tasks[j].UniqueID, "Task: ", tasks[7].UniqueID, "ES: ", tasks[7].EarlyStart, "EF: ", tasks[7].EarlyFinish, "Duration: ", tasks[7].Duration)
 			}
 		}
 		// _ = graph.BFSWithDepth(bp, tasks[i].UniqueID, func(value string, depth int) bool {
@@ -80,31 +100,73 @@ func calculateCriticalComponent(tasks []Task) []Task {
 			// define LS and LF currentPredID the last task
 			tasks[taskIndices[currentPredID[j]]].LateFinish = tasks[taskIndices[currentPredID[j]]].EarlyFinish
 			tasks[taskIndices[currentPredID[j]]].LateStart = tasks[taskIndices[currentPredID[j]]].LateFinish - tasks[taskIndices[currentPredID[j]]].Duration
+			if len(tasks[taskIndices[currentPredID[j]]].Successors) == 0 {
+				tasks[taskIndices[currentPredID[j]]].LateFinish = longestFinish
+				tasks[taskIndices[currentPredID[j]]].LateStart = tasks[taskIndices[currentPredID[j]]].LateFinish - tasks[taskIndices[currentPredID[j]]].Duration
+			}
+			if len(tasks[taskIndices[currentPredID[j]]].Predecessors) > 0 {
+				for _, predecessor := range tasks[taskIndices[currentPredID[j]]].Predecessors {
+					switch predecessor.Type {
+					case "FS":
+					case "FF":
+					case "SF":
+					case "SS":
+						lag := float64(0)
+						if predecessor.Lag != "0" {
+							unit, value := splitLag(predecessor.Lag)
+							lag = float64(value)
+							if unit == "mo" {
+								lag = float64(value) * 30
+							}
+						}
+						if tasks[taskIndices[predecessor.UniqueID]].EarlyFinish > tasks[taskIndices[currentPredID[j]]].LateFinish {
+							tasks[taskIndices[currentPredID[j]]].LateFinish = tasks[taskIndices[predecessor.UniqueID]].EarlyFinish - float64(lag)
+						}
+						tasks[taskIndices[currentPredID[j]]].LateStart = tasks[taskIndices[currentPredID[j]]].LateFinish - tasks[taskIndices[currentPredID[j]]].Duration
+					}
+				}
+			}
 			if len(tasks[taskIndices[currentPredID[j]]].Successors) > 0 {
-				fmt.Println("[ITERATION]", j, currentPredID[j])
 				for _, successor := range tasks[taskIndices[currentPredID[j]]].Successors {
-					tasks[taskIndices[currentPredID[j]]].LateFinish = tasks[taskIndices[successor.UniqueID]].LateStart
-					tasks[taskIndices[currentPredID[j]]].LateStart = tasks[taskIndices[currentPredID[j]]].LateFinish - tasks[taskIndices[currentPredID[j]]].Duration
+					lag := float64(0)
+					if successor.Lag != "0" {
+						unit, value := splitLag(successor.Lag)
+						lag = float64(value)
+						if unit == "mo" {
+							lag = float64(value) * 30
+						}
+					}
+					switch successor.Type {
+					case "FS":
+						tasks[taskIndices[currentPredID[j]]].LateFinish = tasks[taskIndices[successor.UniqueID]].LateStart - float64(lag)
+						tasks[taskIndices[currentPredID[j]]].LateStart = tasks[taskIndices[currentPredID[j]]].LateFinish - tasks[taskIndices[currentPredID[j]]].Duration
+					case "FF":
+					case "SF":
+						tasks[taskIndices[currentPredID[j]]].LateFinish = tasks[taskIndices[successor.UniqueID]].LateFinish
+						if lag > 0 {
+							tasks[taskIndices[currentPredID[j]]].LateFinish = tasks[taskIndices[successor.UniqueID]].LateFinish - float64(lag) - 1
+						}
+						tasks[taskIndices[currentPredID[j]]].LateStart = tasks[taskIndices[currentPredID[j]]].LateFinish - tasks[taskIndices[currentPredID[j]]].Duration
+
+					case "SS":
+					}
 				}
 			}
 			fmt.Println("[ITERATION]", j, "CurrentTask", tasks[j].UniqueID, "Task: ", tasks[0].UniqueID, "LS: ", tasks[0].LateStart, "LF: ", tasks[0].LateFinish, "Duration: ", tasks[0].Duration)
 			fmt.Println("[ITERATION]", j, "CurrentTask", tasks[j].UniqueID, "Task: ", tasks[1].UniqueID, "LS: ", tasks[1].LateStart, "LF: ", tasks[1].LateFinish, "Duration: ", tasks[1].Duration)
 			fmt.Println("[ITERATION]", j, "CurrentTask", tasks[j].UniqueID, "Task: ", tasks[2].UniqueID, "LS: ", tasks[2].LateStart, "LF: ", tasks[2].LateFinish, "Duration: ", tasks[2].Duration)
-			fmt.Println("[ITERATION]", j, "CurrentTask", tasks[j].UniqueID, "Task: ", tasks[4].UniqueID, "LS: ", tasks[4].LateStart, "LF: ", tasks[4].LateFinish, "Duration: ", tasks[4].Duration)
 		}
 	}
 
 	// Step 3: Calculate Total Slack
 	for i := range tasks {
 		tasks[i].TotalSlack = tasks[i].LateFinish - tasks[i].EarlyFinish
+		fmt.Println("[RESULT]", "Task: ", tasks[i].UniqueID, "EF", tasks[i].EarlyFinish, "LS: ", tasks[i].LateStart, "LF: ", tasks[i].LateFinish, "Duration: ", tasks[i].Duration, "TotalSlack: ", tasks[i].TotalSlack)
 		if tasks[i].TotalSlack == 0 {
 			tasks[i].CriticalPath = true
 		}
 	}
+	fmt.Println(longestFinish, "longestSlack")
 
-	// fmt.Println("[RESULT]", "Task: ", tasks[0].UniqueID, "LS: ", tasks[0].LateStart, "LF: ", tasks[0].LateFinish, "Duration: ", tasks[0].Duration)
-	// fmt.Println("[RESULT]", "Task: ", tasks[4].UniqueID, "LS: ", tasks[4].LateStart, "LF: ", tasks[4].LateFinish, "Duration: ", tasks[4].Duration)
-	// fmt.Println("[RESULT]", "Task: ", tasks[6].UniqueID, "LS: ", tasks[6].LateStart, "LF: ", tasks[6].LateFinish, "Duration: ", tasks[6].Duration)
-	// fmt.Println("[RESULT]", "Task: ", tasks[7].UniqueID, "LS: ", tasks[7].LateStart, "LF: ", tasks[7].LateFinish, "Duration: ", tasks[7].Duration)
 	return tasks
 }
--- critical_path_test.go
+++ critical_path_test.go
@@ -10,45 +10,45 @@ func TestCriticalPath(t *testing.T) {
 		TaskCriticalPath := []Task{
 			{UniqueID: "1",
 				Duration:     10,
-				Successors:   []Successor{{UniqueID: "2", Lag: "0"}, {UniqueID: "6", Lag: "0"}},
+				Successors:   []Successor{{UniqueID: "2", Lag: "0", Type: "FS"}, {UniqueID: "6", Lag: "0", Type: "FS"}},
 				Predecessors: []Predecessor{},
 			},
 			{UniqueID: "2",
 				Duration:     5,
-				Successors:   []Successor{{UniqueID: "3", Lag: "0"}, {UniqueID: "8", Lag: "0"}},
-				Predecessors: []Predecessor{{UniqueID: "1", Lag: "0"}},
+				Successors:   []Successor{{UniqueID: "3", Lag: "0", Type: "FS"}, {UniqueID: "8", Lag: "0", Type: "FS"}},
+				Predecessors: []Predecessor{{UniqueID: "1", Lag: "0", Type: "FS"}},
 			},
 			{UniqueID: "3",
 				Duration:     7,
-				Successors:   []Successor{{UniqueID: "4", Lag: "0"}},
-				Predecessors: []Predecessor{{UniqueID: "2", Lag: "0"}},
+				Successors:   []Successor{{UniqueID: "4", Lag: "0", Type: "FS"}},
+				Predecessors: []Predecessor{{UniqueID: "2", Lag: "0", Type: "FS"}},
 			},
 			{UniqueID: "4",
 				Duration:     6,
-				Successors:   []Successor{{UniqueID: "5", Lag: "0"}},
-				Predecessors: []Predecessor{{UniqueID: "3", Lag: "0"}},
+				Successors:   []Successor{{UniqueID: "5", Lag: "0", Type: "FS"}},
+				Predecessors: []Predecessor{{UniqueID: "3", Lag: "0", Type: "FS"}},
 			},
 			{UniqueID: "5",
 				Duration:   4,
 				Successors: []Successor{},
-				Predecessors: []Predecessor{{UniqueID: "4", Lag: "0"},
-					{UniqueID: "7", Lag: "0"},
-					{UniqueID: "8", Lag: "0"}},
+				Predecessors: []Predecessor{{UniqueID: "4", Lag: "0", Type: "FS"},
+					{UniqueID: "7", Lag: "0", Type: "FS"},
+					{UniqueID: "8", Lag: "0", Type: "FS"}},
 			},
 			{UniqueID: "6",
 				Duration:     8,
-				Successors:   []Successor{{UniqueID: "7", Lag: "0"}, {UniqueID: "8", Lag: "0"}},
-				Predecessors: []Predecessor{{UniqueID: "1", Lag: "0"}},
+				Successors:   []Successor{{UniqueID: "7", Lag: "0", Type: "FS"}, {UniqueID: "8", Lag: "0", Type: "FS"}},
+				Predecessors: []Predecessor{{UniqueID: "1", Lag: "0", Type: "FS"}},
 			},
 			{UniqueID: "7",
 				Duration:     4,
-				Successors:   []Successor{{UniqueID: "5", Lag: "0"}},
-				Predecessors: []Predecessor{{UniqueID: "6", Lag: "0"}},
+				Successors:   []Successor{{UniqueID: "5", Lag: "0", Type: "FS"}},
+				Predecessors: []Predecessor{{UniqueID: "6", Lag: "0", Type: "FS"}},
 			},
 			{UniqueID: "8",
 				Duration:     14,
-				Successors:   []Successor{{UniqueID: "5", Lag: "0"}},
-				Predecessors: []Predecessor{{UniqueID: "2", Lag: "0"}, {UniqueID: "6", Lag: "0"}},
+				Successors:   []Successor{{UniqueID: "5", Lag: "0", Type: "FS"}},
+				Predecessors: []Predecessor{{UniqueID: "2", Lag: "0", Type: "FS"}, {UniqueID: "6", Lag: "0", Type: "FS"}},
 			},
 		}
 
@@ -66,57 +66,183 @@ func TestCriticalPath(t *testing.T) {
 	})
 }
 
-func TestCriticalPathMultiDependency(t *testing.T) {
+func TestCriticalPathLag(t *testing.T) {
 
-	t.Run("Test Success Critical Path Multi Dependency", func(t *testing.T) {
+	t.Run("Test Success Critical Path FS", func(t *testing.T) {
 		TaskCriticalPath := []Task{
 			{UniqueID: "1",
-				Duration:     2,
-				Successors:   []Successor{{UniqueID: "2", Lag: "0"}},
+				Duration:     10,
+				Successors:   []Successor{{UniqueID: "2", Lag: "0", Type: "FS"}},
 				Predecessors: []Predecessor{},
 			},
 			{UniqueID: "2",
-				Duration:     10,
-				Successors:   []Successor{{UniqueID: "3", Lag: "0"}, {UniqueID: "7", Lag: "0"}},
-				Predecessors: []Predecessor{{UniqueID: "1", Lag: "0"}},
+				Duration:     5,
+				Successors:   []Successor{{UniqueID: "3", Lag: "10.0d", Type: "FS"}},
+				Predecessors: []Predecessor{{UniqueID: "1", Lag: "0", Type: "FS"}},
 			},
 			{UniqueID: "3",
-				Duration:     14,
-				Successors:   []Successor{{UniqueID: "4", Lag: "0"}, {UniqueID: "6", Lag: "0"}, {UniqueID: "7", Lag: "0"}},
-				Predecessors: []Predecessor{{UniqueID: "2", Lag: "0"}},
+				Duration:     10,
+				Successors:   []Successor{{UniqueID: "4", Lag: "0", Type: "SS"}},
+				Predecessors: []Predecessor{{UniqueID: "2", Lag: "10.0d", Type: "FS"}},
 			},
 			{UniqueID: "4",
-				Duration:     3,
-				Successors:   []Successor{{UniqueID: "5", Lag: "0"}, {UniqueID: "6", Lag: "0"}, {UniqueID: "7", Lag: "0"}},
-				Predecessors: []Predecessor{{UniqueID: "4", Lag: "0"}},
+				Duration:     1,
+				Successors:   []Successor{},
+				Predecessors: []Predecessor{{UniqueID: "3", Lag: "0", Type: "SS"}},
 			},
 			{UniqueID: "5",
-				Duration:     4,
-				Successors:   []Successor{},
-				Predecessors: []Predecessor{{UniqueID: "4", Lag: "0"}},
+				Duration:     2,
+				Successors:   []Successor{{UniqueID: "6", Lag: "2.0d", Type: "SS"}},
+				Predecessors: []Predecessor{},
 			},
 			{UniqueID: "6",
-				Duration:     5,
+				Duration:     3,
 				Successors:   []Successor{},
-				Predecessors: []Predecessor{{UniqueID: "3", Lag: "0"}, {UniqueID: "4", Lag: "0"}},
-			},
-			{UniqueID: "7",
-				Duration:     7,
-				Successors:   []Successor{},
-				Predecessors: []Predecessor{{UniqueID: "2", Lag: "0"}, {UniqueID: "3", Lag: "0"}, {UniqueID: "4", Lag: "0"}},
+				Predecessors: []Predecessor{{UniqueID: "5", Lag: "2.0d", Type: "SS"}},
 			},
 		}
 
 		// Expected Result
-		expected := float64(2)
+		expected := float64(0)
 
 		taskCpm := calculateCriticalComponent(TaskCriticalPath)
 		if taskCpm[0].TotalSlack != expected {
 			t.Errorf("Expected %v but got %v", expected, taskCpm[0].TotalSlack)
 		}
 
-		if taskCpm[6].TotalSlack != float64(0) {
-			t.Errorf("Expected %v but got %v", float64(0), taskCpm[6].TotalSlack)
+		if taskCpm[1].TotalSlack != float64(0) {
+			t.Errorf("Expected %v but got %v", float64(0), taskCpm[1].TotalSlack)
+		}
+
+		// if taskCpm[2].EarlyFinish != float64(35) {
+		// 	t.Errorf("Expected %v but got %v", float64(35), taskCpm[2].EarlyFinish)
+		// }
+		// if taskCpm[3].LateFinish != float64(35) {
+		// 	t.Errorf("Expected %v but got %v", float64(35), taskCpm[3].LateFinish)
+		// }
+		// if taskCpm[3].LateStart != float64(34) {
+		// 	t.Errorf("Expected %v but got %v", float64(34), taskCpm[3].LateStart)
+		// }
+		if taskCpm[3].EarlyFinish != float64(26) {
+			t.Errorf("Expected %v but got %v", float64(26), taskCpm[3].EarlyFinish)
+		}
+		if taskCpm[3].TotalSlack != float64(9) {
+			t.Errorf("Expected %v but got %v", float64(9), taskCpm[3].TotalSlack)
+		}
+
+		if taskCpm[5].TotalSlack != float64(30) {
+			t.Errorf("Expected %v but got %v", float64(30), taskCpm[5].TotalSlack)
+		}
+		if taskCpm[5].EarlyFinish != float64(5) {
+			t.Errorf("Expected %v but got %v", float64(5), taskCpm[5].EarlyFinish)
+		}
+		if taskCpm[5].LateFinish != float64(35) {
+			t.Errorf("Expected %v but got %v", float64(35), taskCpm[5].LateFinish)
+		}
+	})
+}
+
+func TestCriticalPathMultiDependency(t *testing.T) {
+
+	t.Run("Test Success Critical Path Multi Dependency", func(t *testing.T) {
+		TaskCriticalPath := []Task{
+			{UniqueID: "1",
+				Duration:     1,
+				Successors:   []Successor{{UniqueID: "2", Lag: "2.d", Type: "SS"}},
+				Predecessors: []Predecessor{},
+			},
+			{UniqueID: "2",
+				Duration:     1,
+				Successors:   []Successor{{UniqueID: "3", Lag: "14.0d", Type: "FS"}},
+				Predecessors: []Predecessor{{UniqueID: "1", Lag: "2.0d", Type: "SS"}},
+			},
+			{UniqueID: "3",
+				Duration:     1,
+				Successors:   []Successor{},
+				Predecessors: []Predecessor{{UniqueID: "2", Lag: "14.0d", Type: "FS"}},
+			},
+			{UniqueID: "4",
+				Duration:     1,
+				Successors:   []Successor{{UniqueID: "6", Lag: "0", Type: "FS"}},
+				Predecessors: []Predecessor{{UniqueID: "5", Lag: "0", Type: "FF"}},
+			},
+			{UniqueID: "5",
+				Duration:     1,
+				Successors:   []Successor{{UniqueID: "4", Lag: "0", Type: "FF"}},
+				Predecessors: []Predecessor{},
+			},
+			{UniqueID: "6",
+				Duration:     1,
+				Successors:   []Successor{},
+				Predecessors: []Predecessor{{UniqueID: "4", Lag: "0", Type: "FS"}},
+			},
+			{UniqueID: "7",
+				Duration:     1,
+				Successors:   []Successor{{UniqueID: "8", Lag: "0", Type: "SF"}},
+				Predecessors: []Predecessor{},
+			},
+			{UniqueID: "8",
+				Duration:     1,
+				Successors:   []Successor{},
+				Predecessors: []Predecessor{{UniqueID: "7", Lag: "0", Type: "SF"}},
+			},
+			{UniqueID: "9",
+				Duration:     1,
+				Successors:   []Successor{{UniqueID: "10", Lag: "10.0d", Type: "SF"}},
+				Predecessors: []Predecessor{},
+			},
+			{UniqueID: "10",
+				Duration:     1,
+				Successors:   []Successor{},
+				Predecessors: []Predecessor{{UniqueID: "9", Lag: "10.0d", Type: "SF"}},
+			},
+		}
+
+		// Expected Result
+		expected := float64(0)
+
+		taskCpm := calculateCriticalComponent(TaskCriticalPath)
+		if taskCpm[0].TotalSlack != expected {
+			t.Errorf("Expected %v but got %v", expected, taskCpm[0].TotalSlack)
+		}
+
+		if taskCpm[5].TotalSlack != float64(16) {
+			t.Errorf("Expected %v but got %v", float64(16), taskCpm[5].TotalSlack)
+		}
+
+		if taskCpm[6].EarlyStart != float64(0) {
+			t.Errorf("Expected %v but got %v", float64(0), taskCpm[6].EarlyStart)
+		}
+		if taskCpm[6].EarlyFinish != float64(1) {
+			t.Errorf("Expected %v but got %v", float64(1), taskCpm[6].EarlyFinish)
+		}
+		if taskCpm[6].LateStart != float64(17) {
+			t.Errorf("Expected %v but got %v Late Start", float64(16), taskCpm[6].LateStart)
+		}
+		if taskCpm[6].LateFinish != float64(18) {
+			t.Errorf("Expected %v but got %v Late Finish", float64(17), taskCpm[6].LateFinish)
+		}
+		if taskCpm[6].TotalSlack != float64(17) {
+			t.Errorf("Expected %v but got %v slack task 6", float64(17), taskCpm[6].TotalSlack)
+		}
+
+		if taskCpm[7].EarlyStart != float64(-1) {
+			t.Errorf("Expected %v but got %v Early start task 8", float64(-1), taskCpm[7].EarlyStart)
+		}
+		if taskCpm[7].EarlyFinish != float64(0) {
+			t.Errorf("Expected %v but got %v early finish task 8", float64(0), taskCpm[7].EarlyFinish)
+		}
+		if taskCpm[7].LateStart != float64(17) {
+			t.Errorf("Expected %v but got %v Late Start", float64(18), taskCpm[7].LateStart)
+		}
+		if taskCpm[7].LateFinish != float64(18) {
+			t.Errorf("Expected %v but got %v Late Finish", float64(18), taskCpm[7].LateFinish)
+		}
+		if taskCpm[7].TotalSlack != float64(18) {
+			t.Errorf("Expected %v but got %v", float64(18), taskCpm[7].TotalSlack)
+		}
+		if taskCpm[9].TotalSlack != float64(8) {
+			t.Errorf("Expected %v but got %v", float64(8), taskCpm[9].TotalSlack)
 		}
 	})
 }
